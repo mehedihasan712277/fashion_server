df1dab02a41211f080687efde22b5b9e6e624e73
mehedi <mehedihasan712277@gmail.com>
2025-12-09 19:02:21 +0600

    feat: add blog categories

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import userRoutes from "./user.routes";
+import blog_categoryRoutes from "./blog_category.routes";
 
 const router = Router();
 
 router.use("/users", userRoutes);
+router.use("/blog-categories", blog_categoryRoutes);
 
 export default router;
